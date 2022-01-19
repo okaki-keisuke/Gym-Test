acf90f7e6226b1d5884d4befc54a832c1769aae8
okaki-keisuke <okaki.roy1130@gmail.com>
2022-01-19 22:13:45 +0900

    2022/1/19

--- Breakout/utils.py
+++ Breakout/utils.py
@@ -8,19 +8,23 @@ from model import FRAME_HEIGHT, FRAME_WIDTH, INPUT_HEIGHT, INPUT_WIDTH
 def get_initial_state(observation: np) -> torch.Tensor:
         observation = torchvision.transforms.functional.to_tensor(observation)
         observation = observation.reshape(1, 3, FRAME_HEIGHT, FRAME_WIDTH)
-        transform = transforms.Compose([transforms.Resize((INPUT_HEIGHT, INPUT_WIDTH), transforms.InterpolationMode.BICUBIC),
-                                        torchvision.transforms.Grayscale(num_output_channels=1)
+        observation_crop = torchvision.transforms.functional.crop(observation, 34, 0, 160, 160)
+        transform = transforms.Compose([torchvision.transforms.Grayscale(num_output_channels=1),
+                                        transforms.Resize((INPUT_HEIGHT, INPUT_WIDTH), transforms.InterpolationMode.BICUBIC)
                                         ])
-        observation = transform(observation)
-        state = torch.cat((observation, observation, observation, observation), 1)
+        observation_trans = transform(observation_crop)
+        observation_scale = torch.div(observation_trans, 255)
+        state = torch.cat((observation_scale, observation_scale, observation_scale, observation_scale), 1)
         return state
 
 def input_image(observation: torch.Tensor, state: torch.Tensor) -> torch.Tensor:
         observation = torchvision.transforms.functional.to_tensor(observation)
         observation = observation.reshape(1, 3, FRAME_HEIGHT, FRAME_WIDTH)
-        transform = transforms.Compose([transforms.Resize((INPUT_HEIGHT, INPUT_WIDTH), transforms.InterpolationMode.BICUBIC),
-                                        torchvision.transforms.Grayscale(num_output_channels=1)
+        observation_crop = torchvision.transforms.functional.crop(observation, 34, 0, 160, 160)
+        transform = transforms.Compose([torchvision.transforms.Grayscale(num_output_channels=1),
+                                        transforms.Resize((INPUT_HEIGHT, INPUT_WIDTH), transforms.InterpolationMode.BICUBIC)
                                         ])
-        observation = transform(observation)
-        state = torch.cat((observation, state[:, :3, :, :]), 1)
+        observation_trans = transform(observation_crop)
+        observation_scale = torch.div(observation_trans, 255)
+        state = torch.cat((observation_scale, state[:, :3, :, :]), 1)
         return state
